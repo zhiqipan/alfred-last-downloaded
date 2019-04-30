d9ca3c0ec89fb4b0b2983303fd4d793f6909ac83
PAN Zhiqi <zhiqipan@yahoo.com>
2019-04-30 19:28:04 +0800

    Use ls command to replace AppleScript for better performance

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const path = require('path');
 const alfy = require('alfy');
-const runApplescript = require('run-applescript');
+const { exec } = require('child_process');
 
 const run = async () => {
-  const result = await runApplescript﻿(`
-    on run
-      tell application "Finder"
-        set latestFile to last item of (sort (get files of (path to downloads folder)) by creation date) as alias
-        set fileName to latestFile's name
-      end tell
-      return fileName
-      # return POSIX path of latestFile
-    end run
-  `);
-  alfy.output([{
-    title: result,
-    subtitle: '~/Downloads/'+result,
-    arg: '~/Downloads/'+result,
-  }])
+  exec('cd ~/Downloads && ls -ltcr1 | tail -1', ((error, stdout, stderr) => {
+    if (!!stdout) {
+      const result = stdout.trim();
+      alfy.output([{
+        title: result,
+        subtitle: '~/Downloads/' + result,
+        arg: '~/Downloads/' + result,
+      }])
+    }
+  }));
 }
 
 run()
